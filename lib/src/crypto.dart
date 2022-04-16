3db4a6446537b66a259a4cf87a186c93b23e0d59
ethicnology <ethicnology@pm.me>
2022-04-16 17:32:36 +0200

    feat: wrap external crypto functions pbkdf2 and sha256 from pointycastle

---
+++ lib/src/crypto.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:pointycastle/digests/sha512.dart';
+import 'package:pointycastle/key_derivators/api.dart';
+import 'package:pointycastle/key_derivators/pbkdf2.dart';
+import 'package:pointycastle/macs/hmac.dart';
+import 'package:pointycastle/digests/sha256.dart';
+
+List<int> pbkdf2(List<int> entropy, {String passphrase = ""}) {
+  const blockLength = 128;
+  const iterationCount = 2048;
+  const desiredKeyLength = 64;
+  final salt = Uint8List.fromList(utf8.encode("mnemonic" + passphrase));
+  final derivator = PBKDF2KeyDerivator(HMac(SHA512Digest(), blockLength));
+  derivator.reset();
+  derivator.init(Pbkdf2Parameters(salt, iterationCount, desiredKeyLength));
+  Uint8List result = derivator.process(Uint8List.fromList(entropy));
+  return result.toList();
+}
+
+List<int> sha256(List<int> bytes) {
+  final digest = SHA256Digest();
+  Uint8List result = digest.process(Uint8List.fromList(bytes));
+  return result.toList();
+}
