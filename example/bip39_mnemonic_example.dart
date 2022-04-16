24f59356d27d147536b5169725e745d30091c17c
ethicnology <ethicnology@pm.me>
2022-04-16 17:18:43 +0200

    feat: dart package-simple template

---
+++ example/bip39_mnemonic_example.dart
@@ -0,0 +1,6 @@
+import 'package:bip39_mnemonic/bip39_mnemonic.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/bip39_mnemonic.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library bip39_mnemonic;
+
+export 'src/bip39_mnemonic_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
