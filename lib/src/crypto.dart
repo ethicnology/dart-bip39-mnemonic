3ad9678b0b06dfa8ae5da9271f95437dc6f850ae
ethicnology <ethicnology@pm.me>
2022-04-17 19:17:18 +0200

    fix: sentence.codeunits -> utf8

--- lib/src/crypto.dart
+++ lib/src/crypto.dart
@@ -14,11 +14,13 @@ List<int> pbkdf2(String sentence, {String passphrase = ""}) {
   const blockLength = 128;
   const iterationCount = 2048;
   const desiredKeyLength = 64;
-  final salt = Uint8List.fromList(utf8.encode("mnemonic" + passphrase));
+  final salt = Uint8List.fromList(utf8.encode("mnemonic$passphrase"));
   final derivator = PBKDF2KeyDerivator(HMac(SHA512Digest(), blockLength));
   derivator.reset();
   derivator.init(Pbkdf2Parameters(salt, iterationCount, desiredKeyLength));
-  Uint8List result = derivator.process(Uint8List.fromList(sentence.codeUnits));
+  Uint8List result = derivator.process(
+    Uint8List.fromList(utf8.encode(sentence)),
+  );
   return result.toList();
 }
 
