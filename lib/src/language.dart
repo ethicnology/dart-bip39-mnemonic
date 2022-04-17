fa5582dadaaaa2ca363d659b9017b9c5173f31d6
ethicnology <ethicnology@pm.me>
2022-04-17 11:01:37 +0200

    refactor: language is required to constructs Mnemonic, fromSentence verify external checksum

--- lib/src/language.dart
+++ lib/src/language.dart
@@ -1,6 +1,9 @@
 import 'wordlists/french.dart';
 import 'wordlists/english.dart';
 
+/// BIP39:
+/// * Since the vast majority of BIP39 wallets supports only the English wordlist, it is strongly discouraged to use non-English wordlists for generating the mnemonic sentences.
+/// * If you still feel your application really needs to use a localized wordlist, use one of the following instead of inventing your own.
 enum Language { french, english }
 
 extension LanguageExtension on Language {
