c0712fb44f1ad22d85b0d60ee6a75ee8987e8e88
ethicnology <ethicnology@pm.me>
2022-04-18 11:20:48 +0200

    chore: update examples and documentation

--- example/bip39_mnemonic_example.dart
+++ example/bip39_mnemonic_example.dart
@@ -1,26 +1,32 @@
 import 'package:bip39_mnemonic/bip39_mnemonic.dart';
-import 'package:convert/convert.dart';
+import 'package:convert/convert.dart'; // to convert bytes to hexadecimal and vice-versa
 
 void main() {
   // DO NOT USE THESE DATA!
+
+  // Constructs Mnemonic from random secure 256bits entropy
+  var mnemonic = Mnemonic.generate(Language.french);
+  print(mnemonic.sentence);
+  // million alpaga revivre calmer dogme verdure capsule folie déborder facette lanceur saboter recycler tripler symbole savant rieur jeudi outrager volume situer jardin civil reculer
+  List<int> bytesSeed = mnemonic.toSeed(passphrase: "GPLv3");
+  String hexSeed = hex.encode(bytesSeed); // convert to hex
+  print(hexSeed);
+  // 9097c95aa6a100792973041c3701bff43bfcd39995fc75da0ec56bd74111fcb7759752b08040bc10db537fe86d4bcb44b95c450b04a9a9c3913e247f4f99e9e8
+
+  // Constructs a Mnemonic from Entropy
   String hexEntropy =
       "677765ded2b5f05fd11d9bcc38b863fda3fa06475c77bf1e99a668af355b96e2";
   List<int> bytesEntropy = hex.decode(hexEntropy);
-
-  var mnemonic = Mnemonic(bytesEntropy, Language.english);
-  print(mnemonic.sentence);
+  var mnemonic2 = Mnemonic(bytesEntropy, Language.english);
+  print(mnemonic2.sentence);
   // guess robot jeans pistol gallery copper dutch recall slow shift body win distance add buddy moment sample visit hat spend viable punch fortune faith
 
-  List<int> bytesSeed = mnemonic.toSeed();
-  String hexSeed = hex.encode(bytesSeed);
-  print(hexSeed);
-  // 1cbea444d42a98554a6984956509a19992ba500b30a789f7959bdb719c88d8e3ddd4c84522d60e2ac83591fc32c774e02a9719ea2bf4233c7a456f286b2cc0eb
-
+// Constructs a Mnemonic from Sentence/Phrase
   String frenchSentence =
       "image juteux calculer billard valise billard tortue besace peigne corbeau adroit littoral";
-  var mnemonic2 = Mnemonic.fromSentence(frenchSentence, Language.french);
-  print(hex.encode(mnemonic2.entropy));
+  var mnemonic3 = Mnemonic.fromSentence(frenchSentence, Language.french);
+  print(hex.encode(mnemonic3.entropy));
   // 7e71249c8ebf603afb68e1b4c6fc18c8
-  print(hex.encode(mnemonic2.toSeed()));
+  print(hex.encode(mnemonic3.toSeed()));
   // ad926f1f185ba7745f2c98733a83e51717a58ec83ef52c5cd12048aa8fbe4b2511cf12c2a514d2886510f7020b8a0c1c75bedacfbb3b34cd2f3d8d2c038d531e
 }
