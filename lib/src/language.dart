e2af3fde5fa8ea15ff92d6533d66cdceed55c115
ethicnology <ethicnology@pm.me>
2022-05-28 11:43:29 +0200

    refactor: file based wordlists introduced path nightmare  -> Revert "refactor: language enum enhanced, wordlists .dart -> .txt"
    Language enum is still enhanced with RAM loaded wordlists.
    
    introducing new Language attribute, label :
    Language.french.label returns french

--- lib/src/language.dart
+++ lib/src/language.dart
@@ -1,37 +1,36 @@
-import 'dart:io';
-import 'package:path/path.dart' as path;
+import 'package:bip39_mnemonic/src/wordlists/french.dart';
+import 'package:bip39_mnemonic/src/wordlists/english.dart';
+import 'package:bip39_mnemonic/src/wordlists/spanish.dart';
+import 'package:bip39_mnemonic/src/wordlists/italian.dart';
+import 'package:bip39_mnemonic/src/wordlists/czech.dart';
+import 'package:bip39_mnemonic/src/wordlists/portuguese.dart';
+import 'package:bip39_mnemonic/src/wordlists/korean.dart';
+import 'package:bip39_mnemonic/src/wordlists/chinese_simplified.dart';
+import 'package:bip39_mnemonic/src/wordlists/chinese_traditional.dart';
+import 'package:bip39_mnemonic/src/wordlists/japanese.dart';
 
 /// BIP39:
 /// * Since the vast majority of BIP39 wallets supports only the English wordlist, it is strongly discouraged to use non-English wordlists for generating the mnemonic sentences.
 /// * If you still feel your application really needs to use a localized wordlist, use one of the following instead of inventing your own.
 enum Language {
-  french('french'),
-  english('english'),
-  spanish('spanish'),
-  italian('italian'),
-  czech('czech'),
-  portuguese('portuguese'),
-  korean('korean'),
-  simplifiedChinese('chinese_simplified'),
-  traditionalChinese('chinese_traditional'),
-  japanese('japanese');
+  french('french', frenchList),
+  english('english', englishList),
+  spanish('spanish', spanishList),
+  italian('italian', italianList),
+  czech('czech', czechList),
+  portuguese('portuguese', portugueseList),
+  korean('korean', koreanList),
+  simplifiedChinese('chinese simplified', simplifiedChineseList),
+  traditionalChinese('chinese traditional', traditionalChineseList),
+  japanese('japanese', japaneseList);
 
   final String _label;
-  const Language(this._label);
+  final List<String> _list;
+  const Language(this._label, this._list);
 
   String get label => _label;
 
-  List<String> get list {
-    List<String> wordlist = [];
-    var context = path.Context();
-    var absolute = Directory.current.absolute.path;
-    var wordlistFile = File(context.join('$absolute/wordlists/', '$label.txt'));
-    List<String>? lines = wordlistFile.readAsLinesSync();
-    for (var word in lines) {
-      wordlist.add(word);
-    }
-    return wordlist;
-  }
+  List<String> get list => _list;
 
   Map<int, String> get map => list.asMap();
 
