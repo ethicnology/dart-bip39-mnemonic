7b0a26855617767e092407c33d33cfad11ab679c
ethicnology <ethicnology@pm.me>
2022-05-30 21:55:43 +0200

    refactor: Language enum order place english on top to encourage users to use english mnemonic. label is no longer private (in case user want to change language label)

--- lib/src/language.dart
+++ lib/src/language.dart
@@ -9,26 +9,23 @@ import 'package:bip39_mnemonic/src/wordlists/chinese_simplified.dart';
 import 'package:bip39_mnemonic/src/wordlists/chinese_traditional.dart';
 import 'package:bip39_mnemonic/src/wordlists/japanese.dart';
 
-/// BIP39:
-/// * Since the vast majority of BIP39 wallets supports only the English wordlist, it is strongly discouraged to use non-English wordlists for generating the mnemonic sentences.
-/// * If you still feel your application really needs to use a localized wordlist, use one of the following instead of inventing your own.
+/// BIP39: Since the vast majority of BIP39 wallets supports only the English wordlist, it is strongly discouraged to use non-English wordlists for generating the mnemonic sentences.
+/// If you still feel your application really needs to use a localized wordlist, use one of the following instead of inventing your own.
 enum Language {
-  french('french', frenchList),
   english('english', englishList),
+  french('french', frenchList),
   spanish('spanish', spanishList),
   italian('italian', italianList),
-  czech('czech', czechList),
   portuguese('portuguese', portugueseList),
+  czech('czech', czechList),
   korean('korean', koreanList),
   simplifiedChinese('chinese simplified', simplifiedChineseList),
   traditionalChinese('chinese traditional', traditionalChineseList),
   japanese('japanese', japaneseList);
 
-  final String _label;
+  final String label;
   final List<String> _list;
-  const Language(this._label, this._list);
-
-  String get label => _label;
+  const Language(this.label, this._list);
 
   List<String> get list => _list;
 
