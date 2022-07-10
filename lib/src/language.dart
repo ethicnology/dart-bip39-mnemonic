6f99d6a5b5f7ccdd295f6a342615cc59fbb6dcef
ethicnology <ethicnology@pm.me>
2022-07-10 22:17:06 +0200

    refactor: isValid function formats (NFKD) inputs such as BIP39 PBKDF2

--- lib/src/language.dart
+++ lib/src/language.dart
@@ -8,6 +8,7 @@ import 'package:bip39_mnemonic/src/wordlists/korean.dart';
 import 'package:bip39_mnemonic/src/wordlists/chinese_simplified.dart';
 import 'package:bip39_mnemonic/src/wordlists/chinese_traditional.dart';
 import 'package:bip39_mnemonic/src/wordlists/japanese.dart';
+import 'package:unorm_dart/unorm_dart.dart';
 
 /// BIP39: Since the vast majority of BIP39 wallets supports only the English wordlist, it is strongly discouraged to use non-English wordlists for generating the mnemonic sentences.
 /// If you still feel your application really needs to use a localized wordlist, use one of the following instead of inventing your own.
@@ -39,8 +40,8 @@ enum Language {
     }
   }
 
-  /// isValid checks the provided word presence in the wordlist
+  /// isValid function formats input (NFKD) and check his existence in the current language wordlist
   bool isValid(String word) {
-    return list.contains(word);
+    return list.contains(nfkd(word));
   }
 }
